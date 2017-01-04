cef6fd966a339762c4821f7d318660cbe9d51786
BramGoderie <bram.goderie@incentro.com>
2017-01-04 15:58:46 +0100

    additional commit

--- LunchIncentro/Controllers/VestigingOverzichtController.cs
+++ LunchIncentro/Controllers/VestigingOverzichtController.cs
@@ -15,16 +15,14 @@ namespace LunchIncentro.Controllers
         public static PayChoice ChosenPayChoice = PayChoice.Nothing;
         private static string vestigingIdSaver = "";
         // GET: VestigingOverzicht
-        public ActionResult Index(string vestigingID, string payChoice)
+        public ActionResult Index(string vestigingID)
         {
             if (vestigingID != null) vestigingIdSaver = vestigingID;
             else vestigingID = vestigingIdSaver;
             var vestigingen = DependencyResolver.Current.GetService<VestigingsController>()
                 .GetVestigingenWithBalance(User.Identity.GetUserName());
             var vestigingOverzicht = vestigingen.FirstOrDefault(v => v.Vestiging.Id.Equals(vestigingID));
-            ViewBag.ChosenPayChoice = ChosenPayChoice;
             ViewBag.payChoice = new SelectList(Enum.GetNames(typeof(PayChoice)));
-            ChosenPayChoice = (PayChoice)Enum.Parse(typeof(PayChoice),payChoice);
 
             return View(vestigingOverzicht);
         }
